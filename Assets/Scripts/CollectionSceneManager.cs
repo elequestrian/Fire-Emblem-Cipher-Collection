922f4e21e7124bb2939a804bb34275956cfdc385
Theron Calkins <61123120+elequestrian@users.noreply.github.com>
2023-11-10 07:36:08 -0500

    Adding Scrollbar functionality

--- Assets/Scripts/CollectionSceneManager.cs
+++ Assets/Scripts/CollectionSceneManager.cs
@@ -1,52 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Com.SakuraStudios.FECipherCollection
 {
     public class CollectionSceneManager : MonoBehaviour
     {
+        [SerializeField] private Scrollbar cardListScrollbar;
         [SerializeField] private Transform cardGridTransform;
 
         // Position variables
         float initialX = -6.1f;
         float initialY = 3.1f;
         float incrementX = 2.5f;
-        float incrementY = -3.4f;
+        float incrementY = 3.4f;
         int maxColumns = 4;
-
+        int rowCounter = 1;
 
         #region Unity Callbacks
 
         // Start is called before the first frame update
         void Start()
         {
+            
+            //Populate card list method
             Vector3 newCardPosition = new Vector3(initialX, initialY, 0);
             int columnCounter = 1;
+            
 
             // Create and position all cards 
             foreach (CipherData.CardID cardID in CipherData.CardID.GetValues(typeof(CipherData.CardID)))
             {
+                //check if we need to move back to the first column for the next card
+                if (columnCounter > maxColumns)
+                {
+                    newCardPosition.x = initialX;
+                    newCardPosition.y -= incrementY;
+                    columnCounter = 1;
+                    rowCounter++;
+                }
+
                 LoadCard(cardID, cardGridTransform, newCardPosition);
 
                 newCardPosition.x += incrementX;
                 columnCounter++;
 
-                //check if we need to move back to the first column
-                if (columnCounter > maxColumns)
-                {
-                    newCardPosition.x = initialX;
-                    newCardPosition.y += incrementY;
-                    columnCounter = 1;
-                }
-                
             }
+
+            /*
+            //Setup Scrollbar method
+            if (rowCounter <= 2)
+                cardListScrollbar.size = 1;
+            else if (rowCounter == 3)
+                cardListScrollbar.size = 0.95f;
+            else if (rowCounter >= 4)
+                cardListScrollbar.size = 0.9f;
+            */
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void MoveCardGrid(float scrollbarValue)
+        {
+            Vector3 newPosition = cardGridTransform.localPosition;
+
+            //calculate the max value for the Card Grid object.
+            float maxGridY = 0.5f + ((rowCounter - 3) * incrementY);
+            if (maxGridY < 0)
+                maxGridY = 0;
+
+            //Set the y position to a value between 0 and a max based on the number of rows.
+            newPosition.y = scrollbarValue * maxGridY;
+
+            cardGridTransform.localPosition = newPosition;
         }
 
         #endregion
